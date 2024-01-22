e481f2f8cbad414aca24c263d6b689ccdb371309
VlDTenshi <118188335+VlDTenshi@users.noreply.github.com>
2024-01-23 01:58:34 +0200

    Working with objects

---
+++ CarCatalogyProject/Data/Repository/CarRepository.cs
@@ -0,0 +1,22 @@
+﻿using CarCatalogyProject.Data.interfaces;
+using CarCatalogyProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarCatalogyProject.Data.Repository
+{
+    public class CarRepository : IAllCars
+    {
+        private readonly AppDBContent appDBContent;
+
+        public CarRepository(AppDBContent appDBContent)
+        {
+            this.appDBContent = appDBContent;
+        }
+        public IEnumerable<Car> Cars => appDBContent.Car.Include(c => c.TypeOfCar);
+
+        public IEnumerable<Car> getFavCars => appDBContent.Car.Where(p => p.isFavorite).Include(c => c.TypeOfCar);
+
+        public Car getObjectCar(int carid) => appDBContent.Car.FirstOrDefault(p => p.id == carid);
+        
+    }
+}
---
+++ CarCatalogyProject/Data/Repository/TypeRepository.cs
@@ -0,0 +1,16 @@
+﻿using CarCatalogyProject.Data.interfaces;
+using CarCatalogyProject.Models;
+
+namespace CarCatalogyProject.Data.Repository
+{
+    public class TypeRepository : ITypeOfCar
+    {
+        private readonly AppDBContent appDBContent;
+
+        public TypeRepository(AppDBContent appDBContent)
+        {
+            this.appDBContent = appDBContent;
+        }
+        public IEnumerable<TypeOfCar> AllTypes => appDBContent.TypeOfCar;
+    }
+}
