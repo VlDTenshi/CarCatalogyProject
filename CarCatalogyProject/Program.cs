882e85edf7377900e916fc12b64ef06feeff3ab1
VlDTenshi <118188335+VlDTenshi@users.noreply.github.com>
2024-01-23 01:55:16 +0200

    Some fixes

--- CarCatalogyProject/Program.cs
+++ CarCatalogyProject/Program.cs
@@ -1,27 +1,50 @@
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+using CarCatalogyProject.Data;
+using CarCatalogyProject.Data.interfaces;
+using CarCatalogyProject.Data.Repository;
+using Microsoft.EntityFrameworkCore;
+using System.Drawing.Text;
+public class Program
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    public static async Task Main(string[] args)
+{
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Add services to the container.
+    builder.Services.AddControllersWithViews();
+
+    builder.Services.AddDbContext<AppDBContent>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    builder.Services.AddScoped<IAllCars, CarRepository>();
+    builder.Services.AddScoped<ITypeOfCar, TypeRepository>();
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+        await ConfigureAsync(app, builder.Environment);
+        app.Run();
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
+    private static async Task ConfigureAsync(WebApplication app, IHostEnvironment env)
+    {
+        using (var scope = app.Services.CreateScope())
+        {
+            var content = scope.ServiceProvider.GetRequiredService<AppDBContent>();
+            await DBObject.Initial(content);
+        }
+    }
+}
