33e75b867a98b557b7d95136cc6968833adebe53
VlDTenshi <118188335+VlDTenshi@users.noreply.github.com>
2024-01-23 01:56:33 +0200

    Creating classes in models

---
+++ CarCatalogyProject/Models/Contact.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CarCatalogyProject.Models
+{
+    public class Contact
+    {
+        [Display(Name= "Insert a name")]
+        [Required(ErrorMessage = "You have to insert a name")]
+        public string Name { get; set; }
+        [Display(Name = "Insert a surname")]
+        [Required(ErrorMessage = "You have to insert a surname")]
+        public string Surname { get; set; }
+        [Display(Name = "Insert an age")]
+        [Required(ErrorMessage = "You have to insert an Age")]
+        public int Age { get; set; }
+        [Display(Name = "Insert an Email")]
+        [Required(ErrorMessage = "You have to insert an Email")]
+        public string Email { get; set; }
+        [Display(Name = "Insert an Message")]
+        [Required(ErrorMessage = "You have to insert an Message")]
+        [StringLength(30, ErrorMessage = "Text should be not less than 30 characters")]
+        public string Message { get; set; }
+    }
+}
---
+++ CarCatalogyProject/Models/TypeOfCar.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace CarCatalogyProject.Models
+{
+    public class TypeOfCar
+    {
+        public int id {  get; set; }
+        public string typeName { get; set; }
+        public string description { get; set; }
+        public List<Car> cars { get; set;}
+    }
+}
