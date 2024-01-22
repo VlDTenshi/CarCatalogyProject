4c2e5697bee127277991c805ab3e6d9937d3a5ab
VlDTenshi <118188335+VlDTenshi@users.noreply.github.com>
2024-01-23 01:54:44 +0200

    List

---
+++ CarCatalogyProject/ViewModels/CarsListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using CarCatalogyProject.Models;
+using System.Collections.Generic;
+
+namespace CarCatalogyProject.ViewModels
+{
+    public class CarsListViewModel
+    {
+        public IEnumerable<Car> allCars {  get; set; }
+
+        public string carType { get; set; }
+    }
+}
