e62bf889990dcb066b41067017bd67cf25e2ed6c
VlDTenshi <118188335+VlDTenshi@users.noreply.github.com>
2024-01-23 02:15:31 +0200

    Controller

---
+++ CarCatalogyProject/Controllers/CarsController.cs
@@ -0,0 +1,28 @@
+﻿using CarCatalogyProject.Data.interfaces;
+using CarCatalogyProject.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarCatalogyProject.Controllers
+{
+    public class CarsController : Controller
+    {
+        private readonly IAllCars _allCars;
+        private readonly ITypeOfCar _allTypes;
+
+        public CarsController(IAllCars allCars, ITypeOfCar allTypes)
+        {
+            _allCars = allCars;
+            _allTypes = allTypes;
+        }
+
+        public ViewResult List()
+        {
+            ViewBag.Title = "The page with vehicles";
+            CarsListViewModel model = new CarsListViewModel();
+            model.allCars = _allCars.Cars;
+            model.carType = "Vehicles";
+            //var cars = _allCars.Cars;
+            return View(model);
+        }
+    }
+}
