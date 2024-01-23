d0185d92f354801c77158a7a2fed488ff735ff10
VlDTenshi <118188335+VlDTenshi@users.noreply.github.com>
2024-01-23 02:00:06 +0200

    Controllers

---
+++ CarCatalogyProject/Controllers/ContactsController.cs
@@ -0,0 +1,22 @@
+﻿using CarCatalogyProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarCatalogyProject.Controllers
+{
+    public class ContactsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Check(Contact contact)
+        {
+            if(ModelState.IsValid)
+            {
+                return Redirect("/");
+            }
+            return View("Index");
+        }
+    }
+}
